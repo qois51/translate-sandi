2551b6339f055ec8009b9b265bf9da9c0c8784a7
Qois-69912 <qois.firosi@gmail.com>
2021-12-21 18:38:07 +0700

    Agak berantakan, tetapi design mobile selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     content: ["./**/*.html"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                Montserrat: ["Montserrat", "sans-serif"],
+            },
+            colors: {
+                'black': "#2E3440",
+                'black-less': "#4C566A",
+                'white' : "#F7F7F7",
+                'grey' : "#CFCFCF",
+            },
+        },
     },
     plugins: [],
 }
